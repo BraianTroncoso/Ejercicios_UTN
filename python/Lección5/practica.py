788cb1ecf993379b5a9f311e7af502af4b49a839
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2022-11-23 04:05:45 -0300

    Actualizacion ejercicios Python

--- python/Lección5/practica.py
+++ python/Lección5/practica.py
@@ -1,18 +1,1 @@
-"""def cuenta_atras(num):
-    num -= 1
-    if num > 0:
-        print(num)
-        cuenta_atras(num)
-    elif num < 0:
-        print('No imprimo nada')
-    else:
-        print("Boooooooom!")
-
-
-cuenta_atras(6)"""
-factorial = 1
-ingresar = int(input('Ingrese un número para calcular su factorial: '))
-
-resultado = factorial(ingresar) # Lo hacemos en código duro
-print(f'El factorial del número 5 es: {resultado}')
-ingresar2 = int(input('Digite el num: '))
+print(len((1, )))
