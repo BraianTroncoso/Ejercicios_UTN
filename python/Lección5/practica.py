5f4eef41d14546055bdf05b38a7ad74a80de8fb2
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2022-12-27 17:56:04 -0300

    [UPDATE] Refactorización de ejercicios.

--- python/Lección5/practica.py
+++ python/Lección5/practica.py
@@ -1,1 +1,11 @@
-print(len((1, )))
+
+"""class Empleado: # No hereda sino solo de la clase object
+    def __init__(self, nombre, sueldo):
+        self.nombre = nombre
+        self.sueldo = sueldo
+
+        def __str__(self):
+            return f'Empleado: [Nombre: {self.nombre}, Sueldo: {self.sueldo}]'
+
+        def mostrar_detalles(self):
+            return self.__str__()"""
