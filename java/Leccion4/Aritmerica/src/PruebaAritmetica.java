9c5fdc065880b2433b2ac941996cebefbd2e064a
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-01-18 08:08:13 -0300

    [UPDATE] Actualización de clases de JAVA 7.2

--- java/Leccion4/Aritmerica/src/PruebaAritmetica.java
+++ java/Leccion4/Aritmerica/src/PruebaAritmetica.java
@@ -1,6 +1,6 @@
 import Operaciones.Aritmetica;
 
-public class PruebaAritmetica {
+public class PruebaAritmetica { // Una sola clase se puede hacer de tipo publica
     public static void main(String[] args) {
         var a = 10; // variables locales
         int b = 7; // Memoria Stack (Las variables locales trabajan con la memoria Stack)
@@ -26,10 +26,27 @@ public class PruebaAritmetica {
         System.out.println("aritmetica1 a: "+aritmetica2.a);
         System.out.println("aritmetica2 b: "+aritmetica2.b);
 
+
     }
 
+
+/*        Persona persona = new Persona("Ariel","Bentacud");
+        System.out.println("persona = " + persona);
+        System.out.println("persona nombre = " + persona.nombre);
+        System.out.println("persona apellido = " + persona.apellido);*/
+// Modularidad creamos un nuevo método
     public static void miMetodo(){
         // int a = 10; // Esta variable esta limitada
         System.out.println("Aqui hay otro metodo");
     }
+class Persona{ // Se le asigna a la clase un tipo default o package a la clase (pq ya tenemos una publica)
+    String nombre;
+    String apellido;
+
+    Persona (String nombre, String apellido){ // THIS Hace referencia al atributo
+    this.nombre = nombre;// Se guarda los valores indicado en el atributo que llamamos y le asignamos el parametro
+    this.apellido = apellido; // Del constructor, por convenio se usa el mismo nombre asi no se confunda.
+
+    }
+}
 }
