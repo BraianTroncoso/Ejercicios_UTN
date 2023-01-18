55d49ef6d0743331905f07463e03e01156a156f8
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-01-18 06:39:21 -0300

    [UPDATE] Actualización de clases de JAVA

--- java/Practica/Practica/src/Practica.java
+++ java/Practica/Practica/src/Practica.java
@@ -1,3 +1,21 @@
+import javax.swing.*;
 public class Practica {
+    /*
+    Ejercicio 10: Pedir 10 números y escribir la suma total
+    Hacerlo con la clase Scanner y JOptionPane
+     */
+    public static void main(String[] args) {
 
+
+
+        /*        int contador = 0;
+        int suma = 0;
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero "));
+        while (contador <= 9){
+            suma += numero;
+            contador++;
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Digite otro numero "));
+        }
+        JOptionPane.showMessageDialog(null,"La suma del total es: " + suma);*/
+    }
 }
