31bb05ad6b63cc356e4a7937131be79ba6125b3f
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-05-24 04:16:16 -0300

    [ADD] Clase 6 - Js - 6.2 Método Get y Set: Parte Get y Parte Set

--- Tecnicatura3JS/TSLeccion05/05-03-clases.js
+++ Tecnicatura3JS/TSLeccion05/05-03-clases.js
@@ -1,10 +1,20 @@
 class Persona{
     constructor(nombre, apellido){ // Sino se define el constructor js añade uno vacio de forma automatica
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    set nombre(nombre){
         this.nombre = nombre;
-        this.apellido = apellido;
     }
 }
 let persona1 = new Persona ('Martin','Perez');
-console.log(persona1);
+console.log(persona1.nombre);
+persona1.nombre = 'Juan Carlos';
+console.log(persona1.nombre);
 let persona2 = new Persona('Carlos','Lara');
-console.log(persona2);
+persona2.nombre = 'Maria Laura'
+console.log(persona2.nombre);
+
