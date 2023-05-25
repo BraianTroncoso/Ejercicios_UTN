b468d34c585c92aa8f853bc26b7b7e904ebb20e7
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-05-24 22:42:53 -0300

    [ADD] Clase 7 - Java - version demo

---
+++ Tecnicatura3Java/Leccion05/Interfaces/src/test/TestInterfaces.java
@@ -0,0 +1,18 @@
+package test;
+
+import accesodatos.IAccesoDatos;
+import accesodatos.ImplementacionMysql;
+import accesodatos.ImplementacionOracle;
+
+public class TestInterfaces {
+    public static void main(String[] args) {
+        IAccesoDatos datos = new ImplementacionMysql();
+        //datos.listar();
+        //imprimir(datos);
+        datos = new ImplementacionOracle();
+        //imprimir(datos);
+    }
+    public static void imprimir(IAccesoDatos datos){
+        datos.listar();
+    }
+}
