e6498c27b036e1523ca8805ddaa1254c6f7cb66b
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-06-07 15:46:55 -0300

    [ADD] Clase 8 - Python - Completa

---
+++ Tecnicatura3Py/Leccion07/capa_datos_persona/persona_dao.py
@@ -0,0 +1,14 @@
+class PersonaDAO:
+    """
+    DAO significa: Data Acces Object
+    CRUD significa:
+        Create -> Insertar
+        Read -> Seleccionar
+        Update -> Actualizar
+        Delete -> Eliminar
+    """
+
+    _SELECIONAR='SELECT * FROM persona ORDER BY id_persona'
+    _INSERTAR='INSERT INTO persona(nombre, apellido, email) VALUES(%s, %s, %s)'
+    _ACTUALIZAR='UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    _ELIMINAR='DELETE FROM persona WHERE id_persona=%s'
