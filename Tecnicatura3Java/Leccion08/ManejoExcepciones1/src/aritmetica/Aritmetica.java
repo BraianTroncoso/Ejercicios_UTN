3d1e9854dd41ff53b92ab408bc656dcd71cc8eb9
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-06-22 01:22:50 -0300

    [ADD] Clase 8 - Java - 8.4 Creamos nuestra propia excepción -> Parte 2

--- Tecnicatura3Java/Leccion08/ManejoExcepciones1/src/aritmetica/Aritmetica.java
+++ Tecnicatura3Java/Leccion08/ManejoExcepciones1/src/aritmetica/Aritmetica.java
@@ -10,3 +10,6 @@ public class Aritmetica {
         return numerador / denominador;
     }
 }
+// throws = tirar (si ocurre un error, quiero tirar este mensaje)
+// try = intentar (por ejm, problablemente esta funcion pueda arrojar un error por algun motivo)
+// catch = capturar(si ocurre el error lo capturo y digo que haga lo que quiera, para que la app no se detenga y continue el flujo)
