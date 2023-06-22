0f04c9120929dd42cc138aa382e026ce529abde4
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-06-22 01:30:29 -0300

    [ADD] Clase 8 - Java - 8.4 Creamos nuestra propia excepción -> Parte 3

--- Tecnicatura3Java/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Tecnicatura3Java/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -1,14 +1,16 @@
 package test;
 
+import static aritmetica.Aritmetica.division; // Importamos solamente el método de la clase Aritmetica
+
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
-
         try {
-            resultado = 10/0;
+            resultado = division(10,0);
         } catch (Exception e){
             System.out.println("Ocurrió un error");
             e.printStackTrace(System.out); // Se conoce como la pila de excepciones
+            System.out.println(e.getMessage());
         }
         System.out.println("El valor de la variable resultado es: "+resultado);
     }
