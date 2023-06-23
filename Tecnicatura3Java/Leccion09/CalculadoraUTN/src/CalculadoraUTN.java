4ea544fbabb7007412adedf10e097c407f1d47b9
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-06-23 02:47:35 -0300

    [ADD] Clase 9 - Java - 9.2 Definiendo las variables parte 2

---
+++ Tecnicatura3Java/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -0,0 +1,9 @@
+public class CalculadoraUTN {
+    public static void main(String[] args) {
+        System.out.println("******* Aplicación Calculadora *******");
+        var operando1 = 10;
+        var operando2 = 20;
+        var resultado = operando1 + operando2;
+        System.out.println("resultado = " + resultado);
+    }
+}
