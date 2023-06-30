cae6a641ecc7c6ac51369d22b006aaa47c604e5b
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-06-30 02:37:47 -0300

    [ADDD] Clase 12 - Java - 12.2 Listado de estudiantes con la Clase EstudianteDAO -> Parte 3

--- Segundo año/Tecnicatura3Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/Datos/EstudianteDAO.java
+++ Segundo año/Tecnicatura3Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/Datos/EstudianteDAO.java
@@ -1,7 +1,10 @@
 package UTN.Datos;
 
+import UTN.Conexion.Conexion;
 import UTN.Dominio.Estudiante;
+import static UTN.Conexion.Conexion.getConecction;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -14,5 +17,9 @@ public class EstudianteDAO {
         // Creamos ahora algunos objetos que son necesarios para comunicarnos con la BDD
         PreparedStatement ps; // Envia la sentencia a la base de datos(los querys)
         ResultSet rs; // Recibe el resultado de la base de datos
+        // Creamos un objeto del tipo conexion
+        Connection con = getConecction();
+        String sql = "SELECT * FROM"
+
     }
 }
