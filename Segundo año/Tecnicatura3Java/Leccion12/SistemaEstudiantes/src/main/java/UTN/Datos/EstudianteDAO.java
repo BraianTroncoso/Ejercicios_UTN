0c8cdca6835f2aa3d067fa4382241604db31b5ed
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-03 04:54:55 -0300

    [ADDD] Clase 12 - Java - 12.2 Listado de estudiantes con la Clase EstudianteDAO -> Parte 4 y 5

--- Segundo año/Tecnicatura3Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/Datos/EstudianteDAO.java
+++ Segundo año/Tecnicatura3Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/Datos/EstudianteDAO.java
@@ -20,7 +20,29 @@ public class EstudianteDAO {
         // Creamos un objeto del tipo conexion
         Connection con = getConecction();
         String sql = "SELECT * FROM estudiantes ORDEN BY estudiantes2022";
-
-
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()){
+               var estudiante = new Estudiante();
+               estudiante.setIdEstudiante(rs.getInt("idestudiante2022"));
+               estudiante.setNombre(rs.getString("nombre"));
+               estudiante.setApellido(rs.getString("apellido"));
+               estudiante.setEmail(rs.getString("email"));
+               estudiante.setTelefono(rs.getString("telefono"));
+               //Falta agregarlo a la lista
+               estudiantes.add(estudiante);
+            }
+        }catch (Exception e){
+            System.out.println("Ocurrió un error al seleccionar datos: "+e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            } catch (Exception e){
+                System.out.println("Ocurrió un error al cerrar la conexión");
+            }
+        }
+    return estudiantes;
     }
 }
