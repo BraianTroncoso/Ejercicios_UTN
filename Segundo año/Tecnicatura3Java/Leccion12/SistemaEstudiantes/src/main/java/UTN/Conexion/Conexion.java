2cdcadb44229865259a0b7e439ab65e3c0fedd09
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-07-03 07:01:41 -0300

    [ADDD] Clase 12 - 12.5 Hacemos las pruebas del método -> buscarEstudiantePorId()

--- Segundo año/Tecnicatura3Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/Conexion/Conexion.java
+++ Segundo año/Tecnicatura3Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/Conexion/Conexion.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
             var baseDatos = "estudiantes";
             var url = "jdbc:mysql://localhost:3306/"+baseDatos;
             var usuario = "root";
-            var password = "braianmysql";
+            var password = "";
 
             // Cargamos la clase del driver de mysql en memoria
             try {
