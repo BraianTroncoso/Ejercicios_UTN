f448fb0c094ce347777e3e5c449b484998355bd8
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-14 20:35:35 -0300

    [ADD] 1.1 Comenzamos con el laboratorio de usuario: como tarea se debe crear el UML

---
+++ Segundo año/Tecnicatura3Py/laboratorio_usuarios/Usuario.py
@@ -0,0 +1,35 @@
+class Usuario:
+    def __init__(self, id_usuario=None, username=None, password=None):
+        self._id_usuario = id_usuario
+        self._username = username
+        self._password = password
+    def __str__(self):
+        return f'''
+            Id Usuario: {self._id_usuario},
+            Username: {self._username},
+            Password: {self._password}
+        '''
+
+    
+    @property
+    def id_usuario(self):
+        return self._id_usuario
+    @id_persona.setter
+    def id_usuario(self, id_usuario):
+        self._id_usuario = id_usuario
+
+    @property
+    def username(self):
+        return self._username
+    @nombre.setter
+    def username(self, username):
+        self._username = username
+
+    @property
+    def password(self):
+        return self._password
+    @apellido.setter
+    def password(self, password):
+        self._password = password
+    
+
