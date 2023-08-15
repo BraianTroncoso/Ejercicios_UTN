935c09ffdef406ec23ee8ce71f5ab7693b20d8c6
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-14 22:49:01 -0300

    [ADD] 1.7 Prueba final

---
+++ Segundo año/Tecnicatura3Py/laboratorio_usuarios/menu_app_usuario.py
@@ -0,0 +1,12 @@
+opcion = None
+
+while opcion != 5:
+    print('Opciones: ')
+    print('1. Listar usuarios')
+    print('2. Agregar usuario')
+    print('3. Modificar usuario')
+    print('4. Eliminar usuario')
+    print('5. Salir')
+    opcion = int(input('Digite una opción del (1-5): '))
+
+
