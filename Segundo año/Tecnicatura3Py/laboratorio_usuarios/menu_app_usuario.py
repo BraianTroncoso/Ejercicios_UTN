23b88324c015c219ac71598023408d92ffe4be1c
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-08-14 23:40:06 -0300

    [ADD] 1.7 Prueba final

--- Segundo año/Tecnicatura3Py/laboratorio_usuarios/menu_app_usuario.py
+++ Segundo año/Tecnicatura3Py/laboratorio_usuarios/menu_app_usuario.py
@@ -16,12 +16,28 @@ while opcion != 5:
         for usuario in usuarios:
             log.info(usuario)
 
-    elif opcion ==2:
+    elif opcion == 2:
         username_var = input('Inserte el username: ')
         password_var = input('Inserte el password: ')
         usuario = Usuario(username = username_var, password= password_var)
-        usuario_insertado = UsuarioDAO(usuario)
+        usuario_insertado = UsuarioDAO.insertar(usuario)
         log.info(f'Usuario insertado correctamente: {usuario_insertado}')
+
+    elif opcion == 3:
+        id_usuario_var  = int(input('Digite el ID del usuario a modificar: '))
+        username_var = int(input('Digite el nombre del usuario a modificar: '))
+        password_var = int(input('Digite la contraseña del usuario a modificar: '))
+
+        usuario = Usuario(id_usuario=id_usuario_var, username=username_var, password= password_var)
+        usuario_actualizado = UsuarioDAO.actualizar(usuario)
+        log.info(f'Usuario modificado {usuario_actualizado}')
+
+    elif opcion == 4:
+        id_usuario_var = int(input('Digite el ID del usuaro a eliminar: '))
+
+        usuario = Usuario(id_usuario=id_usuario_var)
+        usuario_eliminado = UsuarioDAO.eliminar(usuario)
+        log.info(f'Usuario eliminado: {usuario_eliminado}')
 
 else:
     log.info('Salimos de la aplicación, Hasta pronto')
