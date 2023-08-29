69b7acd4cb1825be0b1e79b0614b7d3b2b1ff4f6
Braian <troncosobraianaxel@gmail.com>
2023-08-29 01:40:04 -0300

    [ADD]2.2 Terminamos con un hola mundo y añadimos buena información sobre Node

--- Segundo año/CuartoSemestre/MetodologiaDeSistemas/Node/01-Hola-Mundo/app.js
+++ Segundo año/CuartoSemestre/MetodologiaDeSistemas/Node/01-Hola-Mundo/app.js
@@ -1,5 +1,7 @@
-let nombre = 'Juan'
-console.log(nombre);
+const nombre1 = 'Juan'
+console.log(nombre1);
 
-nombre = 'Ariel'
-console.log(nombre)
+const nombre2 = 'Ariel'
+console.log(nombre2)
+
+console.log('Hola mundo desde node')
