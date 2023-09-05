eb05c3ae19edf6f865e255a395560da2180c6243
Braian <troncosobraianaxel@gmail.com>
2023-09-05 01:53:35 -0300

    [ADD] 1.3 Concatenación de String

--- Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase 001/tipo_str.py
+++ Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase 001/tipo_str.py
@@ -1,3 +1,5 @@
+import math
+
 # Profundizando en el tipo String
 
 # Concatenación automática en Python
@@ -6,3 +8,8 @@ variable = ' Adios'
 mensaje = 'Hola'' Alumnos' + variable
 mensaje += ' Terminamos'
 print(mensaje)
+
+# Usamos la clase help para ayuda o documentación (Se conoce como built-in) significa que ya esta incluido en los paquetes.
+help(str)
+help(str.capitalize) # Se ingresa sin () porque no estamos llamando a la funcion.
+help(math) # Se ingresa el modulo para obtener mas informacion, ayuda.
