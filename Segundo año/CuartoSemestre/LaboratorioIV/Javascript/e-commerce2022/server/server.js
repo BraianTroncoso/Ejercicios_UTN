90ed5e35688a060461fb6d72e3ed181b5fbd349a
Braian <troncosobraianaxel@gmail.com>
2023-09-13 01:56:36 -0300

    [ADD] server

--- Segundo año/CuartoSemestre/LaboratorioIV/Javascript/e-commerce2022/server/server.js
+++ Segundo año/CuartoSemestre/LaboratorioIV/Javascript/e-commerce2022/server/server.js
@@ -28,9 +28,9 @@ app.post("/create_preference", (req, res) => {
 			}
 		],
 		back_urls: {
-			"success": "http://localhost:8080/feedback",
-			"failure": "http://localhost:8080/feedback",
-			"pending": "http://localhost:8080/feedback"
+			"success": "http://localhost:8080",
+			"failure": "http://localhost:8080",
+			"pending": ""
 		},
 		auto_return: "approved",
 	};
