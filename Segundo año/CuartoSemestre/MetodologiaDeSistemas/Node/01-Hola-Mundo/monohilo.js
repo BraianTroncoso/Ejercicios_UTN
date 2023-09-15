4af5250f471264b7b05f0d59283b812a8f4d0fc3
Braian <troncosobraianaxel@gmail.com>
2023-09-15 01:53:21 -0300

    [ADD] 5.4 Ejecutamos para aclarar conceptos

--- Segundo año/CuartoSemestre/MetodologiaDeSistemas/Node/01-Hola-Mundo/monohilo.js
+++ Segundo año/CuartoSemestre/MetodologiaDeSistemas/Node/01-Hola-Mundo/monohilo.js
@@ -11,3 +11,8 @@ setInterval(function(){
         var a = 3 + z;
     }
 },1000);
+console.log("Segunda intruccion")
+
+// Node a diferencia de PHP, Python, Java, ES ASINCRONO, en tiempo de ejecucion va leyendo segun
+// definicion, en cambio, los demas lenguajes mencionados si o si es de arriba hacia abajo respetando
+// la jerarquia de ejecucion, node, se salta eso.
