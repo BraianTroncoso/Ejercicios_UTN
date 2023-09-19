b08778ae3379e5e536357e95dd860a0507ada1e2
Braian <troncosobraianaxel@gmail.com>
2023-09-19 03:45:30 -0300

    [ADD] 5.1 Documentación -> doc string

---
+++ Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/doc_str.py
@@ -0,0 +1,7 @@
+'''
+Podemos crear documentacion
+'''
+
+"""
+Estas son las maneras de documentar
+"""
---
+++ Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/mi_clase.py
@@ -0,0 +1,20 @@
+class mi_clase:
+    '''
+    Este es un ejemplo de la documentacion de esta clase
+    es solo para saber que aqui esta mi clase.
+    '''
+    def __init__(self):
+        '''
+        Aqui esta el metodo dunder init
+        Est método es el inicio
+        para esta clase
+        '''
+        def mi_metodo(self,param1,param2):
+            '''
+            Dectectó los parametros automaticamente
+            Este es el método creado por mi que recibe parametros
+            :param param1: Este es el parametro numero 1
+            :param param2: Este es el parametro numero 2
+            :return: Este es el valor del retorno
+            '''
+
