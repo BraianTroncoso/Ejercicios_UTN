4a24dd417f2dff286bd52de41e373bea1b1eec2d
Braian <troncosobraianaxel@gmail.com>
2023-09-20 01:16:11 -0300

    [ADD]5.4 Método join para colecciones a cadenas -> Parte 1 y 2 - CLase 05 - Python - 4to Semestre

--- Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/metodo_join.py
+++ Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/metodo_join.py
@@ -1,7 +1,18 @@
 #help(str.join)
 
 tupla_str = ('Hola', 'Alumnos', 'Tecnicatura', 'Universitaria')
-print(f'Mensaje: {mensaje}')
-
 mensaje = ' '.join(tupla_str)
 print(f'Mensaje: {mensaje}')
+lista_cursos = ['Java','Python','Angular', 'Sprint']
+mensaje = ', '.join(lista_cursos)
+print(f'Mensaje: {mensaje}')
+
+cadena = 'HolaMundo'
+mensaje = '.'.join(cadena)
+print(f'Mensaje: {mensaje}')
+
+diccionario = {'Nombre': 'Juan', 'Apellido': 'Perez', 'Edad': '18'}
+llaves = '-'.join(diccionario.keys())
+valores = '-'.join(diccionario.values())
+print(f'Llaves: {llaves}, Type: {type(llaves)}')
+print(f'Valores: {valores}, Type: {type(valores)}')
