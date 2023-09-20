f3c43539134f56b30f450d625df5ed50ba0899ce
Braian <troncosobraianaxel@gmail.com>
2023-09-20 01:38:46 -0300

    [ADD]5.5 Método split cadena a lista -> Parte 1 y 2 - CLase 05 - Python - 4to Semestre

--- Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/metodo_split.py
+++ Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/metodo_split.py
@@ -9,7 +9,7 @@ print(f'Lista de cursos: {lista_cursos}')
 print(type(lista_cursos)) # Tipo list
 
 cursos_separados_coma = "Java,Python,Node,Javascript,Sprint"
-lista_cursos = cursos_separados_coma.split(',') # Si no se ingresa la , como separador, split por defecto lo convertiria en 1 solo elemento
+lista_cursos = cursos_separados_coma.split(',', 2) # Si no se ingresa la , como separador, split por defecto lo convertiria en 1 solo elemento
 # Ya que por defecto esta el "espacio" y en el string ingresado no hay ningun espacio sino mas bien una coma.
 print(f'Lista de cursos: {lista_cursos}')
 print(len(lista_cursos))
