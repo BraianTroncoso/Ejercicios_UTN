f98dd394efe01a089f541a9ca6d7cf30d080ae54
Braian <troncosobraianaxel@gmail.com>
2023-09-21 10:55:39 -0300

    [ADD] 5.5 Creación de la carpeta para la base de datos - Javascript - Clase 05 - PERN

--- Segundo año/CuartoSemestre/LaboratorioIV/Javascript/PERN-stack/src/app.mjs
+++ Segundo año/CuartoSemestre/LaboratorioIV/Javascript/PERN-stack/src/app.mjs
@@ -6,6 +6,9 @@ const app = express();
 app.use(morgan("dev"));
 
 app.get("/",(req,res) => res.json({ message: "Bienvenidos a mi app"}))
+app.get("/test",(req,res)=> {
+    res.send("test")
+});
 
 
 export default app;
