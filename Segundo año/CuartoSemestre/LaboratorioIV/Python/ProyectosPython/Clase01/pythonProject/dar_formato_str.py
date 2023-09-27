272992e297302e1057d57f2e310b2b6df50ab5ff
Braian <troncosobraianaxel@gmail.com>
2023-09-27 03:46:51 -0300

    [ADD] 6.1 Parámetro posicional de tipo String - Python - Clase 06

--- Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/dar_formato_str.py
+++ Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/dar_formato_str.py
@@ -4,4 +4,5 @@ nombre = 'Ariel'
 edad = 28
 mensaje_con_formato = 'Mi nombre es %s y tengo %d años' % (nombre, edad)
 # %s es un parametro posicional de tipoprint(mensaje_con_formato) String pero puede haber de otros tipos.
+
 print(mensaje_con_formato)
