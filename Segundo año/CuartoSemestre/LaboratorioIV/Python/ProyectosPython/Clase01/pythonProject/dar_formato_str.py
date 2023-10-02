40d76b04029fc51140b9f39589a0672ba48533ed
Braian <troncosobraianaxel@gmail.com>
2023-10-02 18:52:36 -0300

    [ADD] 6.3 Uso del método format() -> utilizamos place holder  - Python

--- Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/dar_formato_str.py
+++ Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/dar_formato_str.py
@@ -17,5 +17,5 @@ edad = 19
 sueldo = 3000
 #mensaje_con_formato = 'Nombre {} Edad {} Sueldo {:.2f}'.format(nombre,edad,sueldo)
 #print(mensaje_con_formato)
-mensaje = 'Nombre {0} Edad {1} Sueldo {2}'.format(nombre,edad,sueldo)
+mensaje = 'Nombre {0} Edad {1} Sueldo {2}'.format(nombre,edad,sueldo) # Al manejarse con indice, permite cambiar la posicion de los mismos
 print(mensaje)
