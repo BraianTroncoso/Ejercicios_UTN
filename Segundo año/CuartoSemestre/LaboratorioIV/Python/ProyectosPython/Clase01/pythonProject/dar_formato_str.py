a02817f289b8a29772f03e6b2b3f4903a6c5e93c
Braian <troncosobraianaxel@gmail.com>
2023-10-02 18:17:36 -0300

    [ADD] 6.2 Avanzamos desde una tupla - Python

--- Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/dar_formato_str.py
+++ Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/dar_formato_str.py
@@ -5,4 +5,10 @@ edad = 28
 mensaje_con_formato = 'Mi nombre es %s y tengo %d años' % (nombre, edad)
 # %s es un parametro posicional de tipoprint(mensaje_con_formato) String pero puede haber de otros tipos.
 
+# print(mensaje_con_formato)
+
+# Creamos una tupla
+
+persona = ('Carla', 'Gomez', 5000.00)
+mensaje_con_formato = 'Hola mi nombre es %s %s . Tu sueldo es %.2f' % persona
 print(mensaje_con_formato)
