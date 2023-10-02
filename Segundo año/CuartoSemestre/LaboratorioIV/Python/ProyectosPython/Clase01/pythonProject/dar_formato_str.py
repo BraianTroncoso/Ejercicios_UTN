c53dc1a2d785970adfd732e7ede538bb844fca1c
Braian <troncosobraianaxel@gmail.com>
2023-10-02 19:20:12 -0300

    [ADD] 6.3 Uso del método format() -> utilizamos place holder  - Python

--- Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/dar_formato_str.py
+++ Segundo año/CuartoSemestre/LaboratorioIV/Python/ProyectosPython/Clase01/pythonProject/dar_formato_str.py
@@ -17,5 +17,12 @@ edad = 19
 sueldo = 3000
 #mensaje_con_formato = 'Nombre {} Edad {} Sueldo {:.2f}'.format(nombre,edad,sueldo)
 #print(mensaje_con_formato)
-mensaje = 'Nombre {0} Edad {1} Sueldo {2}'.format(nombre,edad,sueldo) # Al manejarse con indice, permite cambiar la posicion de los mismos
+#mensaje = 'Nombre {0} Edad {1} Sueldo {2}'.format(nombre,edad,sueldo) # Al manejarse con indice, permite cambiar la posicion de los mismos
+#print(mensaje)
+mensaje = 'Nombre {n} Edad {e} Sueldo {s:.2f}'.format(n=nombre, e=edad, s=sueldo) # Como si fuese un constructor declaramos variables temporales
+# Para asignarles un valor similar al argumento posicional
+#print(mensaje)
+
+diccionario = {'nombre':'Ivan','edad':35, 'sueldo':8000.00}
+mensaje = 'Nombre: {dic[nombre]} Edad: {dic[edad]} Sueldo: {dic[sueldo]:.2f}'.format(dic=diccionario)
 print(mensaje)
