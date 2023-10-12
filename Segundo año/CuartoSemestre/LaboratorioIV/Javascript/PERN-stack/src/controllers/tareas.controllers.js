48b9605246b3093c0aa2ebd0ea6b8c7bed869453
Braian <troncosobraianaxel@gmail.com>
2023-10-12 01:09:43 -0300

    [ADD] act node

--- Segundo año/CuartoSemestre/LaboratorioIV/Javascript/PERN-stack/src/controllers/tareas.controllers.js
+++ Segundo año/CuartoSemestre/LaboratorioIV/Javascript/PERN-stack/src/controllers/tareas.controllers.js
@@ -1,8 +1,13 @@
 export const listarTarea = (req, res) => res.send('obteniendo tarea unica');
 export const listarTareas = (req, res) => res.send('obteniendo tareas');
 export const crearTarea = (req, res) => {
-    console.log(req.body);
-    res.send('creando tarea');
+    const {titulo, descripcion } = req.body;
+    try{ const (rows) = await pool.query('INSERT INTO tareas ( titulo, descripcion ) VALUES ($1,$2)',[ titulo, descripcion]);
+        console.log(rows)
+        res.send('Creando tareas');
+    }catch(error){
+        console.log("Algo salio mal")
+    }
 }
 export const actualizarTarea = (req, res) => res.send('actualizando tarea unica');
 export const eliminarTarea = (req, res) => res.send('Eliminando tarea unica');
