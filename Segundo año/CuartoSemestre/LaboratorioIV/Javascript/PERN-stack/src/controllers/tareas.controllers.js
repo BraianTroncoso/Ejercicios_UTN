25a6b13728873bbbe1945a8d398bb08ee948e856
Braian <troncosobraianaxel@gmail.com>
2023-10-12 00:58:49 -0300

    [ADD] act node

--- Segundo año/CuartoSemestre/LaboratorioIV/Javascript/PERN-stack/src/controllers/tareas.controllers.js
+++ Segundo año/CuartoSemestre/LaboratorioIV/Javascript/PERN-stack/src/controllers/tareas.controllers.js
@@ -1,5 +1,8 @@
 export const listarTarea = (req, res) => res.send('obteniendo tarea unica');
 export const listarTareas = (req, res) => res.send('obteniendo tareas');
-export const crearTarea = (req, res) => res.send('creando tarea');
+export const crearTarea = (req, res) => {
+    console.log(req.body);
+    res.send('creando tarea');
+}
 export const actualizarTarea = (req, res) => res.send('actualizando tarea unica');
 export const eliminarTarea = (req, res) => res.send('Eliminando tarea unica');
