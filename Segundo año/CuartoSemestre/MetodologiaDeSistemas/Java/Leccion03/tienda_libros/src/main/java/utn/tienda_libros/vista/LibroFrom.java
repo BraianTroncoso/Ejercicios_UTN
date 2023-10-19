7d84897b2a868c2a3058f0eac1012d88be1e2e2d
Braian <troncosobraianaxel@gmail.com>
2023-10-19 02:02:03 -0300

    [ADD] act codigo - java

--- Segundo año/CuartoSemestre/MetodologiaDeSistemas/Java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Segundo año/CuartoSemestre/MetodologiaDeSistemas/Java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class LibroFrom extends JFrame  {
     LibroServicio libroServicio;
@@ -20,5 +21,11 @@ public class LibroFrom extends JFrame  {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(900,700);
+        // Para obtener las dimenciones de la ventana
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth() / 2);
+        int y = (tamanioPantalla.height - getHeight() / 2);
+        setLocation(x,y);
     }
 }
