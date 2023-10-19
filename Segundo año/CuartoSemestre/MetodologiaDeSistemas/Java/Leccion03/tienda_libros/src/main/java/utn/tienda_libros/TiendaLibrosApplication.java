411f3700e035050586fa40b0ec5b3212652f550b
Braian <troncosobraianaxel@gmail.com>
2023-10-19 02:08:04 -0300

    [ADD] act codigo - java

--- Segundo año/CuartoSemestre/MetodologiaDeSistemas/Java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Segundo año/CuartoSemestre/MetodologiaDeSistemas/Java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,7 +1,10 @@
 package utn.tienda_libros;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
@@ -9,5 +12,9 @@ public class TiendaLibrosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaLibrosApplication.class, args);
 	}
-
+	ConfigurableApplicationContext contextoSpring =
+			new SpringApplicationBuilder(TiendaLibrosApplication.class)
+					.headless(false)
+					.web(WebApplicationType.NONE)
+					.run(args);
 }
