bcc4151ef1b5fa84e632828efc49152c960e5d38
Braian <troncosobraianaxel@gmail.com>
2023-11-03 01:36:07 -0300

    [ADD] act code - node - async - await - all

--- Segundo año/CuartoSemestre/MetodologiaDeSistemas/Node/02-Fundamentos_node/async/asyncAwait.js
+++ Segundo año/CuartoSemestre/MetodologiaDeSistemas/Node/02-Fundamentos_node/async/asyncAwait.js
@@ -1,3 +1,5 @@
+// La palabra async NO ES necesarias en las funciones porqué ya son asincronas
+// Igual proyectan una sincrona visual
 async function hola(nombre){ // async puede trabajar sin la necesidad de utilizar el await
     return new Promise(function (resolve, reject) {
         setTimeout(function (){
@@ -28,7 +30,8 @@ async function adios(nombre){
 }
 
 // await hola('Ariel'); // Esto es una mala sintaxis, no sabe que hacer
-
+// await solo es valido dentro de una funcion asincrona
+// Se puede usar async sin el await pero await no se puede usar sin el async
 async function main(){
     let nombre = await hola('Ariel')
     await hablar()
@@ -36,6 +39,8 @@ async function main(){
     await hablar()
     await hablar()
     await adios(nombre)
+    console.log('Termina el proceso...')
 };
-
+console.log('Empezamos el proceso...')
 main();
+console.log('Esta va ser la segunda instrucción')
