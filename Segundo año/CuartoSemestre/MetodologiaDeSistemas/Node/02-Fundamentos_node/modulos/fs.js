63e5c2f284564e8a1b17568870cbc49703c70f72
Braian <troncosobraianaxel@gmail.com>
2023-11-09 00:08:09 -0300

    [ADD] 13.3 Escribir y crear un archivo-13.4 Eliminar un archivo - Node - Clase 13 - Ultima

--- Segundo año/CuartoSemestre/MetodologiaDeSistemas/Node/02-Fundamentos_node/modulos/fs.js
+++ Segundo año/CuartoSemestre/MetodologiaDeSistemas/Node/02-Fundamentos_node/modulos/fs.js
@@ -8,4 +8,24 @@ function leer(ruta, cb){
     })
 }
 
-leer(`${__dirname}/archivo.txt`,console.log); // Sintaxis ES6
+function escribir(ruta, contenido){
+    fs.writeFile(ruta, contenido, function(err){
+        if(err){
+            console.log('No se ha podido escribir :(',err);
+        }else{
+            console.log('Se ha escrito correctamente :D');
+        }
+    })
+}
+// Tercero eliminamos el archivo1.txt
+function borrar(ruta,cb){
+    fs.unlink(ruta,cb); // Elimina de manera asincrona
+    // Es unlink porque asi se hacia referencia cuando no borra de manera directa sino su referencia en memoria
+    // Por eso no utilizamos delete
+}
+
+//borrar(`${__dirname}/archivo1.txt`,console.log);
+//escribir(`${__dirname}/archivo1.txt`,'Soy un nuevo archivo', console.log) // La funcion writeFile lo que hace
+// Es sobre escribir el archivo en el cual escribe, si le paso una leyenda esta misma sobreescribira la que estaba anteriormente.
+//leer(`${__dirname}/archivo.txt`,console.log); // Sintaxis ES6
+
